865358bb86c610715ebc47e2f249a444c93e0f59
Teepluss <admin@jquerytips.com>
2014-04-11 16:23:45 +0700

    ok

--- src/config/explore.php
+++ src/config/explore.php
@@ -4,7 +4,7 @@ return array(
 
     'endpoint' => 'http://learn.loc/api',
 
-    'prepends'   => array(
+    'prepends' => array(
         array(
             'group'       => 'Parameter',
             'type'        => 'Number',
